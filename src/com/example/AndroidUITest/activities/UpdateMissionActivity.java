90ed419874e6fb99e54bc31be33aca83d90fcac8
Olivier SCHNEIDER <oliwan3dfx@gmail.com>
2013-06-12 11:46:41 +0200

    Add sliding menu

--- src/com/example/AndroidUITest/activities/UpdateMissionActivity.java
+++ src/com/example/AndroidUITest/activities/UpdateMissionActivity.java
@@ -1,14 +1,42 @@
 package com.example.AndroidUITest.activities;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.ListView;
 import com.example.AndroidUITest.R;
+import com.example.AndroidUITest.adapters.MenuAdapter;
+import com.example.AndroidUITest.models.MenuItem;
+import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
 import com.jeremyfeinstein.slidingmenu.lib.app.SlidingActivity;
 
-public class UpdateMissionActivity extends SlidingActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class UpdateMissionActivity extends Activity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.update_mision);
+
+        SlidingMenu slidingMenu = new SlidingMenu(this);
+        slidingMenu.setMode(SlidingMenu.LEFT);
+        slidingMenu.attachToActivity(this, SlidingMenu.SLIDING_CONTENT);
+        slidingMenu.setBehindWidth(300);
+        slidingMenu.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
+        slidingMenu.setMenu(R.layout.hidden_menu);
+        ListView list = (ListView) slidingMenu.getMenu().findViewById(R.id.hiddenMenu);
+        List<MenuItem> menu = new ArrayList<MenuItem>();
+        menu.add(new MenuItem("Mission"));
+        menu.add(new MenuItem("Personnel"));
+        menu.add(new MenuItem("Evènements"));
+        menu.add(new MenuItem("Victimes"));
+        menu.add(new MenuItem("Véhicules"));
+        menu.add(new MenuItem("Retour à la liste"));
+        list.setAdapter(new MenuAdapter(getBaseContext(), menu));
     }
 }
---
+++ src/com/example/AndroidUITest/adapters/MenuAdapter.java
@@ -0,0 +1,39 @@
+package com.example.AndroidUITest.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+import com.example.AndroidUITest.R;
+import com.example.AndroidUITest.models.MenuItem;
+import com.example.AndroidUITest.models.Mission;
+
+import java.util.List;
+
+public class MenuAdapter extends ArrayAdapter<MenuItem> {
+    private List<MenuItem> values;
+    private Context context;
+
+    public MenuAdapter (Context context, List<MenuItem> objects) {
+        super(context, R.layout.hidden_menu_item, objects);
+        this.values = objects;
+        this.context = context;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View rowView = inflater.inflate(R.layout.hidden_menu_item, parent, false);
+        TextView idView = (TextView) rowView.findViewById(R.id.text);
+        ImageView imageView = (ImageView) rowView.findViewById(R.id.image);
+        MenuItem currentItem = values.get(position);
+        String name = currentItem.getName();
+        idView.setText(name);
+        imageView.setImageResource(R.drawable.ic_launcher);
+        return rowView;
+    }
+}
